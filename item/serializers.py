1a8e953184b0b1dd2678484a44cd66b2308ff69b
m.usman <m.usman@tekboox.net>
2025-03-10 16:33:33 +0500

    fix serializer

--- item/serializers.py
+++ item/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from item.models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', allow_null=True, allow_blank=True, required=False)
 
     class Meta:
         model = Item
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'category', 'category_name']
 
