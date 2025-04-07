7807c85f9242c7edd77806c9dd124b9715434e43
m.usman <m.usman@tekboox.net>
2025-04-07 15:27:03 +0500

    change filed in expense model

--- expense/models.py
+++ expense/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class Expense(models.Model):
     item = models.ForeignKey('item.Item', on_delete=models.SET_NULL, null=True, db_column='item')
+    category = models.ForeignKey('category.Category', on_delete=models.SET_NULL, null=True, db_column='category')
     quantity = models.IntegerField(default=None, null=True, db_column='quantity')
     name = models.CharField(max_length=255, db_column='name')
-    description = models.TextField(null=True, db_column='description')
+    description = models.TextField(null=True, blank=True, db_column='description')
     price = models.IntegerField(null=False, db_column='price')
     weight = models.FloatField(default=None, null=True, db_column='weight')
     weight_unit = models.CharField(choices=[('kg', 'kg'), ('g', 'g'), ('ml', 'ml')], null=True, default=None, db_column='weight_unit', max_length=5)
--- expense/serializers.py
+++ expense/serializers.py
@@ -3,13 +3,12 @@ from expense.models import Expense
 
 class ExpenseSerializer(serializers.ModelSerializer):
     item_name = serializers.CharField(source='item.name', allow_null=True, allow_blank=True, required=False)
-    category_id = serializers.CharField(source='item.category.id', allow_null=True, allow_blank=True, required=False)
-    category_name = serializers.CharField(source='item.category.name', allow_null=True, allow_blank=True, required=False)
+    category_name = serializers.CharField(source='category.name', allow_null=True, allow_blank=True, required=False)
     
     class Meta:
         model = Expense
         fields = '__all__'
-        ['item', 'quantity', 'name', 'description', 'price', 'weight', 'weight_unit', 'date', 'item_name',
+        ['item','category', 'quantity', 'name', 'description', 'price', 'weight', 'weight_unit', 'date', 'item_name',
          'category_id', 'category_name']
         
         
